b480526e0695ed03e020f78bd7ea25086494df2f
Victoriano <victorianodev@gmail.com>
2024-11-14 21:49:09 -0600

    clase login

---
+++ MEDIASAFE/src/mediasafe/Login.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mediasafe;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Login {
+    private String name;
+    private int age;
+    private String mail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    
+    
+}
